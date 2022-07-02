d96fa8aa13eabb4c70e59daa75e642802c204d66
langxiong <2926816345@qq.com>
2022-07-02 17:19:06 +0800

    0.0.1

---
+++ src/main/java/com/example/demo4/test.java
@@ -0,0 +1,19 @@
+package com.example.demo4;
+class Bag{
+    String barand;
+    public Bag(String barand){
+      barand=barand;
+    }
+
+}
+
+
+public class test {
+
+
+    public static void main(String[] args) {
+        Bag b=new Bag("niam的");
+        System.out.println(b.barand);
+
+    }
+}
